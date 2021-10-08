4aad786365f602e09f67dc75ffb7cd46df10b5cb
richardingwe <richardingwe5@gmail.com>
2021-10-08 09:21:13 +0100

    added navbar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import { Switch, Route, Link } from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
 
+import { Navbar } from './components';
+
 function App() {
   return (
     <div className="app">
       <div className="navbar">
-
+        <Navbar />
       </div>
       <div className="main">
 
