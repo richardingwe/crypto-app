480330909884bfb9d97b5b6279ce411aa9b77b03
richardingwe <richardingwe5@gmail.com>
2021-10-11 02:28:30 +0100

    added footer

--- src/App.js
+++ src/App.js
@@ -32,9 +32,17 @@ function App() {
             </Switch>
           </div>
         </Layout>
-      </div>
-      <div className="footer">
-
+        <div className="footer">
+          <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>
+            CryptoVerse <br />
+            All rights reserved
+          </Typography.Title>
+          <Space>
+            <Link to='/'>Home</Link>
+            <Link to='/exchanges'>Exchanges</Link>
+            <Link to='/news'>News</Link>
+          </Space>
+        </div>
       </div>
     </div>
   );
