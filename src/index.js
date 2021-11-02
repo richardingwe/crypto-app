5517212ccdcf31af6b9991a3e6bd78250719867c
richardingwe <richardingwe5@gmail.com>
2021-11-02 21:56:39 +0100

    wrapped the whole app with redux provider

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import 'antd/dist/antd.css';
+import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
+
+import App from './App';
+import store from './app/store';
+
+import 'antd/dist/antd.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
